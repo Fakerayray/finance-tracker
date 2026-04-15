a535f1071bbd250b707b2013253986eed33de5d4
Fakerayray <raymondsinglaire@gmail.com>
2026-04-15 10:09:16 +0800

    Add .gitignore and fix Nginx routing for Capstone 2

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,9 @@
-from flask import Flask\napp = Flask(__name__)\n@app.route('/')\ndef hello(): return 'API Online'\nif __name__ == '__main__': app.run(host='0.0.0.0', port=5000)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'API Online'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- vault/app.py
+++ vault/app.py
@@ -1,1 +1,9 @@
-from flask import Flask\napp = Flask(__name__)\n@app.route('/')\ndef hello(): return 'Vault Online'\nif __name__ == '__main__': app.run(host='0.0.0.0', port=8080)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Vault Online'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
